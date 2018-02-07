8acaa0f8e67b2019829bad5e40b0de09f9816410
Mark Palmeri <mlp6@duke.edu>
2018-02-07 10:35:34 -0500

    add initial bolus of files

---
+++ do_stuff.py
@@ -0,0 +1,8 @@
+def sum_list(number_list):
+    """sum all the numbers in number_list
+    """
+    import numpy as np
+
+    s = np.sum(number_list)
+
+    return s
---
+++ test_do_stuff.py
@@ -0,0 +1,4 @@
+def test_sum_list():
+    from do_stuff import sum_list
+    s = sum_list([1, 2, 3])
+    assert s == 6
